ea607996fac5493b87bcffabd2499b6dba34a236
LAPTOP-NKURMQNE\j8801 <E64062092@gs.ncku.edu.tw>
2020-08-05 20:52:23 +0800

    j[]=>cityName[] age24 =>age19to24

--- csv_crawler.py
+++ csv_crawler.py
@@ -1,3 +1,4 @@
+# -*- coding: cp950 -*-
 import requests
 import csv
 import matplotlib.pyplot as plt
@@ -11,10 +12,10 @@ import adjustText as aT
 url = 'http://data.moi.gov.tw/MoiOD/System/DownloadFile.aspx?DATA=C4A5FB3B-1807-4741-A1C0-CF73E49220D7'
 data = requests.get(url, verify=False)
 result = list(csv.reader(StringIO(data.text)))
-j = ['æ–°åŒ—å¸‚ New Taipei', 'è‡ºåŒ—å¸‚ Taipei', 'æ¡ƒåœ’å¸‚ Taoyuan', 'è‡ºä¸­å¸‚ Taichung', 'è‡ºå—å¸‚ Tainan', 'é«˜é›„å¸‚ Kaohsiung', 'å®œè˜­ç¸£ Yilan',
-     'æ–°ç«¹ç¸£ Hsinchu', 'è‹—æ —ç¸£ Miaoli', 'å½°åŒ–ç¸£ Changhua', 'å—æŠ•ç¸£ Nantou', 'é›²æ—ç¸£ Yunlin', 'å˜‰ç¾©ç¸£ Chiayi', 'å±æ±ç¸£ Pingtung', 'è‡ºæ±ç¸£ Taitung',
-     'èŠ±è“®ç¸£ Hualien', 'æ¾æ¹–ç¸£ Penghu', 'åŸºéš†å¸‚ Keelung','æ–°ç«¹å¸‚ Hsinchu','å˜‰ç¾©å¸‚ Chiayi', 'é‡‘é–€ç¸£ Kinmen', 'é€£æ±Ÿç¸£ Lianjiang']
-Doc = 0
+cityName = ['·s¥_¥« New Taipei', '»O¥_¥« Taipei', '®ç¶é¥« Taoyuan', '»O¤¤¥« Taichung', '»O«n¥« Tainan', '°ª¶¯¥« Kaohsiung', '©yÄõ¿¤ Yilan',
+     '·s¦Ë¿¤ Hsinchu', '­]®ß¿¤ Miaoli', '¹ü¤Æ¿¤ Changhua', '«n§ë¿¤ Nantou', '¶³ªL¿¤ Yunlin', '¹Å¸q¿¤ Chiayi', '«ÌªF¿¤ Pingtung', '»OªF¿¤ Taitung',
+     'ªá½¬¿¤ Hualien', '¼ê´ò¿¤ Penghu', '°ò¶©¥« Keelung','·s¦Ë¥« Hsinchu','¹Å¸q¥« Chiayi', 'ª÷ªù¿¤ Kinmen', '³s¦¿¿¤ Lianjiang']
+Doctor = 0
   master = 0
 college = 0
 Acollege = 0
@@ -25,7 +26,7 @@ k = -1
 x = -1
 age15 = 0
 age19 = 0
-age24 = 0
+age19to24 = 0
 age29 = 0
 age34 = 0
 age39 = 0
@@ -54,14 +55,14 @@ locAge=[]
 ##
 
 def printresult(k, Doc, master, college, Acollege, high, sec, ele):
-    print(j[k],'\n')
-    print('åšå£«ç•¢æ¥­:', Doc, 'äºº')
-    print('ç¢©å£«ç•¢æ¥­:', master, 'äºº')
-    print('å¤§å­¸ç•¢æ¥­:', college, 'äºº')
-    print('å°ˆç§‘ç•¢æ¥­:', Acollege, 'äºº')
-    print('é«˜ä¸­ç•¢æ¥­:', high, 'äºº')
-    print('åœ‹ä¸­ç•¢æ¥­:', sec, 'äºº')
-    print('åœ‹å°ä»¥ä¸‹ç•¢æ¥­:', ele, 'äºº')
+    print(ciytName[k],'\n')
+    print('³Õ¤h²¦·~:', Doc, '¤H')
+    print('ºÓ¤h²¦·~:', master, '¤H')
+    print('¤j¾Ç²¦·~:', college, '¤H')
+    print('±M¬ì²¦·~:', Acollege, '¤H')
+    print('°ª¤¤²¦·~:', high, '¤H')
+    print('°ê¤¤²¦·~:', sec, '¤H')
+    print('°ê¤p¥H¤U²¦·~:', ele, '¤H')
     DOC.append(Doc)
     MASTER.append(master)
     COLLEGE.append(college)
@@ -72,66 +73,66 @@ def printresult(k, Doc, master, college, Acollege, high, sec, ele):
     loc.append(Doc+master+college+Acollege+high+sec+ele)
     print()
     
-def printage(age15,age19,age24,age29,age34,age39,age44,age49,age50):
-    print('æœªæ»¿15æ­²:', age15, 'äºº')
-    print("15ï½19æ­²:", age19, 'äºº')
-    print("20ï½24æ­²:", age24, 'äºº')
-    print("25ï½29æ­²:", age29, 'äºº')
-    print("30ï½34æ­²:", age34, 'äºº')
-    print("35ï½39æ­²:", age39, 'äºº')
-    print("40ï½44æ­²:", age44, 'äºº')
-    print("45ï½49æ­²:", age49, 'äºº')
-    print("50æ­²ä»¥ä¸Š:", age50, 'äºº')
+def printage(age15,age19,age19to24,age29,age34,age39,age44,age49,age50):
+    print('¥¼º¡15·³:', age15, '¤H')
+    print("15¡ã19·³:", age19, '¤H')
+    print("20¡ã24·³:", age19to24, '¤H')
+    print("25¡ã29·³:", age29, '¤H')
+    print("30¡ã34·³:", age34, '¤H')
+    print("35¡ã39·³:", age39, '¤H')
+    print("40¡ã44·³:", age44, '¤H')
+    print("45¡ã49·³:", age49, '¤H')
+    print("50·³¥H¤W:", age50, '¤H')
     Age15.append(age15)
     Age19.append(age19)
-    Age24.append(age24)
+    Age24.append(age19to24)
     Age29.append(age29)
     Age34.append(age34)
     Age39.append(age39)
     Age44.append(age44)
     Age49.append(age49)
     Age50.append(age50)
-    locAge.append(age15+age19+age24+age29+age34+age39+age44+age49+age50)
+    locAge.append(age15+age19+age19to24+age29+age34+age39+age44+age49+age50)
 
 for i in range(2,len(result)):
-    if result[i][6] == 'åšå£«ç•¢æ¥­' and int(result[i][7]) > 0:
+    if result[i][6] == '³Õ¤h²¦·~' and int(result[i][7]) > 0:
         Doc += int(result[i][7])
-    elif result[i][6] == 'ç¢©å£«ç•¢æ¥­' and int(result[i][7]) > 0:
+    elif result[i][6] == 'ºÓ¤h²¦·~' and int(result[i][7]) > 0:
         master += int(result[i][7])
-    elif result[i][6] == 'å¤§å­¸ç•¢æ¥­' and int(result[i][7]) > 0:
+    elif result[i][6] == '¤j¾Ç²¦·~' and int(result[i][7]) > 0:
         college += int(result[i][7])
-    elif result[i][6] == 'å°ˆç§‘ç•¢æ¥­' and int(result[i][7]) > 0:
+    elif result[i][6] == '±M¬ì²¦·~' and int(result[i][7]) > 0:
         Acollege += int(result[i][7])
-    elif result[i][6] == 'é«˜ä¸­ç•¢æ¥­' and int(result[i][7]) > 0:
+    elif result[i][6] == '°ª¤¤²¦·~' and int(result[i][7]) > 0:
         high += int(result[i][7])
-    elif result[i][6] == 'åœ‹ä¸­ç•¢æ¥­' and int(result[i][7]) > 0:
+    elif result[i][6] == '°ê¤¤²¦·~' and int(result[i][7]) > 0:
         sec += int(result[i][7])
-    elif result[i][6] == 'åœ‹å°ç•¢æ¥­ä»¥ä¸‹' and int(result[i][7]) > 0:
+    elif result[i][6] == '°ê¤p²¦·~¥H¤U' and int(result[i][7]) > 0:
         ele += int(result[i][7])
 
-    if result[i][5] == 'æœªæ»¿15æ­²' and int(result[i][7]) > 0:
+    if result[i][5] == '¥¼º¡15·³' and int(result[i][7]) > 0:
         age15 += int(result[i][7])
-    elif result[i][5] == '15ï½19æ­²' and int(result[i][7]) > 0:
+    elif result[i][5] == '15¡ã19·³' and int(result[i][7]) > 0:
         age19 += int(result[i][7])
-    elif result[i][5] == '20ï½24æ­²' and int(result[i][7]) > 0:
-        age24 += int(result[i][7])
-    elif result[i][5] == '25ï½29æ­²' and int(result[i][7]) > 0:
+    elif result[i][5] == '20¡ã24·³' and int(result[i][7]) > 0:
+        age19to24 += int(result[i][7])
+    elif result[i][5] == '25¡ã29·³' and int(result[i][7]) > 0:
         age29 += int(result[i][7])
-    elif result[i][5] == '30ï½34æ­²' and int(result[i][7]) > 0:
+    elif result[i][5] == '30¡ã34·³' and int(result[i][7]) > 0:
         age34 += int(result[i][7])
-    elif result[i][5] == '35ï½39æ­²' and int(result[i][7]) > 0:
+    elif result[i][5] == '35¡ã39·³' and int(result[i][7]) > 0:
         age39 += int(result[i][7])
-    elif result[i][5] == '40ï½44æ­²' and int(result[i][7]) > 0:
+    elif result[i][5] == '40¡ã44·³' and int(result[i][7]) > 0:
         age44 += int(result[i][7])
-    elif result[i][5] == '45ï½49æ­²' and int(result[i][7]) > 0:
+    elif result[i][5] == '45¡ã49·³' and int(result[i][7]) > 0:
         age49 += int(result[i][7])
-    elif result[i][5] == '50æ­²ä»¥ä¸Š' and int(result[i][7]) > 0:
+    elif result[i][5] == '50·³¥H¤W' and int(result[i][7]) > 0:
         age50 += int(result[i][7])
     if i!=len(result)-1:
         if result[i][2][0:2] != result[i+1][2][0:2]:
             k = k + 1
             printresult(k, Doc, master, college, Acollege, high, sec, ele)
-            printage(age15, age19, age24, age29, age34, age39, age44, age49, age50)
+            printage(age15, age19, age19to24, age29, age34, age39, age44, age49, age50)
             print('\n')
             Doc = 0
             master = 0
@@ -142,7 +143,7 @@ for i in range(2,len(result)):
             ele = 0
             age15 = 0
             age19 = 0
-            age24 = 0
+            age19to24 = 0
             age29 = 0
             age34 = 0
             age39 = 0
@@ -152,10 +153,10 @@ for i in range(2,len(result)):
     else:
         k = k + 1
         printresult(k, Doc, master, college, Acollege, high, sec, ele)
-        printage(age15, age19, age24, age29, age34, age39, age44, age49, age50)
+        printage(age15, age19, age19to24, age29, age34, age39, age44, age49, age50)
         print('\n\n')
 
-#%% å¹´é½¡å±¤çš„é•·æ¢åœ–
+#%% ¦~ÄÖ¼hªºªø±ø¹Ï
 myfont = FontProperties(fname='C:\\Users\\Peixin\\Downloads\\csv_crawler\\NotoSerifCJKtc-Light.otf')
 X = [12,17,22,27,32,37,42,47,52]
 Y = [sum(Age15),sum(Age19),sum(Age24),sum(Age29),sum(Age34),sum(Age39),sum(Age44),sum(Age49),sum(Age50)]
@@ -163,21 +164,21 @@ G = ["<15","15~19","20~24","25~29","30~34","35~39","40~44","45~49",">=50"]
 
 plt.figure(figsize=(8,4))
 plt.bar(X,height=Y,width=4,color="lightblue",tick_label=G)
-plt.xlabel("å¹´é½¡å±¤",fontproperties = myfont,size=12)
-plt.ylabel("äººæ•¸",fontproperties = myfont,size=12)
-plt.title("108å¹´æ–°ç”Ÿå…’æ¯è¦ªå¹´é½¡å±¤åˆ†ä½ˆ",fontproperties = myfont,size=15)
+plt.xlabel("¦~ÄÖ¼h",fontproperties = myfont,size=12)
+plt.ylabel("¤H¼Æ",fontproperties = myfont,size=12)
+plt.title("108¦~·s¥Í¨à¥À¿Ë¦~ÄÖ¼h¤À§G",fontproperties = myfont,size=15)
 plt.savefig('1.jpg', dpi=300)
-#%% å¹´é½¡å±¤çš„åœ“é¤…åœ–
+#%% ¦~ÄÖ¼hªº¶ê»æ¹Ï
 Y = [sum([sum(Age15),sum(Age19),sum(Age49),sum(Age50)]),sum(Age24),sum(Age29),sum(Age34),sum(Age39),sum(Age44)]
 G = ["<20 and >=45","20~24","25~29","30~34","35~39","40~44"]
 color=["#CCEEFF","#77DDFF","#33CCFF","#0088A8","#009FCC","#00BBFF"]
 
 plt.pie(Y,labels=G,colors=color,explode=(0.1,0,0,0,0,0),autopct="%1.1f%%")
-plt.title("108å¹´æ–°ç”Ÿå…’æ¯è¦ªå¹´é½¡å±¤åˆ†ä½ˆæ¯”ä¾‹åœ–",fontproperties = myfont,size=13)
+plt.title("108¦~·s¥Í¨à¥À¿Ë¦~ÄÖ¼h¤À§G¤ñ¨Ò¹Ï",fontproperties = myfont,size=13)
 plt.axis("equal")
 plt.savefig('2.jpg', dpi=300)
 
-#%% å­¸æ­·çš„é•·æ¢åœ–
+#%% ¾Ç¾úªºªø±ø¹Ï
 #plt.rcParams['font.sans-serif'] = 'C:\\Users\\Peixin\\Downloads\\csv_crawler\\NotoSerifCJKtc-Light.otf'
 X = [10,20,30,40,50,60,70]
 Y = [sum(DOC),sum(MASTER),sum(COLLEGE),sum(ACOLLEGE),sum(HIGH),sum(SEC),sum(ELE)]
@@ -185,41 +186,41 @@ G = ["PhD","master's","Bachelor's","Associate","high","middle","elementary"]
 
 plt.figure(figsize=(8,4))
 plt.bar(X,height=Y,width=5,color="pink",tick_label=G) #orientation = horizontal
-plt.xlabel("å­¸æ­·",fontproperties = myfont,size=12)
-plt.ylabel("äººæ•¸",fontproperties = myfont,size=12)
-plt.title("108å¹´æ–°ç”Ÿå…’æ¯è¦ªå­¸æ­·åˆ†ä½ˆ",fontproperties = myfont,size=15)
+plt.xlabel("¾Ç¾ú",fontproperties = myfont,size=12)
+plt.ylabel("¤H¼Æ",fontproperties = myfont,size=12)
+plt.title("108¦~·s¥Í¨à¥À¿Ë¾Ç¾ú¤À§G",fontproperties = myfont,size=15)
 plt.savefig('3.jpg', dpi=300)
 
-#%% å­¸æ­·çš„åœ“é¤…åœ–
+#%% ¾Ç¾úªº¶ê»æ¹Ï
 Y = [sum(DOC),sum(MASTER),sum(COLLEGE),sum(ACOLLEGE),sum(HIGH),sum(SEC),sum(ELE)]
 G = ["PhD","master's degree","Bachelor's degree","Associate degree","senior high school","middle school","elementary\nschool"]
 color=["#C63300","#FFA488","#FF8888","#FF7744","#FF5511","#E63F00","#FFCCCC"]
 
 plt.pie(Y,labels=G,colors = color,explode=(0,0,0,0,0,0,0.3),autopct="%1.1f%%")
-plt.title("108å¹´æ–°ç”Ÿå…’æ¯è¦ªå­¸æ­·æ¯”ä¾‹åœ–",fontproperties = myfont,size=13)
+plt.title("108¦~·s¥Í¨à¥À¿Ë¾Ç¾ú¤ñ¨Ò¹Ï",fontproperties = myfont,size=13)
 plt.axis("equal")
 plt.savefig('4.jpg', dpi=300)
 
-#%% å‡ºç”ŸåŸå¸‚ Top 7é•·æ¢åœ–
+#%% ¥X¥Í«°¥« Top 7ªø±ø¹Ï
 Dict={}
 Y=[]
 G=[]
 for i in range(len(j)):
-    Dict["{0}".format(j[i])] = loc[i]
+    Dict["{0}".format(cityName[i])] = loc[i]
 y = sorted(Dict.items(), key=lambda x:x[1],reverse=True)
 for i in range(7):
     Y.append(y[i][1])
     G.append(y[i][0])
-G = ['New Taipei','Taoyuan', 'Taichung', 'Taipei', 'Kaohsiung','Changhua', 'Tainan'] #é¡¯ç¤ºä¸äº†ä¸­æ–‡QQ
+G = ['New Taipei','Taoyuan', 'Taichung', 'Taipei', 'Kaohsiung','Changhua', 'Tainan'] #Åã¥Ü¤£¤F¤¤¤åQQ
 
 plt.figure(figsize=(8,4))
 plt.bar(X,height=Y,width=4,color="#CCBBFF",tick_label=G) #orientation = horizontal tick_label=j
-plt.xlabel("åŸå¸‚",fontproperties = myfont,size=13)
-plt.ylabel("äººæ•¸",fontproperties = myfont,size=13)
-plt.title("108å¹´æ–°ç”Ÿå…’å‡ºç”ŸåŸå¸‚ Top 7",fontproperties = myfont,size=15)
+plt.xlabel("«°¥«",fontproperties = myfont,size=13)
+plt.ylabel("¤H¼Æ",fontproperties = myfont,size=13)
+plt.title("108¦~·s¥Í¨à¥X¥Í«°¥« Top 7",fontproperties = myfont,size=15)
 plt.savefig('5.jpg', dpi=300)
 
-#%% åŒ—ä¸­å—æ±é›¢å³¶ åœ“é¤…åœ–
+#%% ¥_¤¤«nªFÂ÷®q ¶ê»æ¹Ï
 N = loc[0]+loc[1]+loc[2]+loc[6]+loc[7]+loc[17]+loc[18]
 W = loc[3]+loc[8]+loc[9]+loc[10]
 S = loc[4]+loc[5]+loc[11]+loc[12]+loc[13]+loc[19]
@@ -230,16 +231,16 @@ G = ["North","West","South","East","offshore islands"]
 color=["#FFEE99","#FFDD55","#FFCC22","#EEEE00","#DDAA00"]
 
 plt.pie(Y,labels=G,colors = color,explode=(0,0,0,0,0.3),autopct="%1.1f%%")
-plt.title("108å¹´å„åœ°å€æ–°ç”Ÿå…’æ¯”ä¾‹",fontproperties = myfont,size=13,loc="right")
+plt.title("108¦~¦U¦a°Ï·s¥Í¨à¤ñ¨Ò",fontproperties = myfont,size=13,loc="right")
 plt.axis("equal")
 plt.savefig('6.jpg', dpi=300)
-#%% å°ç£å„ç¸£å¸‚åœ°åœ–
+#%% ¥xÆW¦U¿¤¥«¦a¹Ï
 county_shp = gpd.read_file(r'D:\NCKU\108-2\Coding-X_Python\map2\COUNTY_MOI_1081121.shp')
 #county_shp.plot(cmap='RdBu')
 
 num = [145,3053,12249,3135,4194,4706,2062,3871,20986,27965,21209,11711,22493,3050,4422,1676,2937,1194,14317,1467,2411,988]
 county_shp['number'] = np.array(num)
-county_shp = county_shp.drop([18],axis=0) #æŠŠé«˜é›„åˆªæ‰
+county_shp = county_shp.drop([18],axis=0) #§â°ª¶¯§R±¼
 county_shp["center"] = county_shp["geometry"].centroid
 TW_points = county_shp.copy()
 TW_points.set_geometry("center", inplace=True)
